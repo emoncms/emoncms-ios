68c24332a3fb4d5b1086a3c3fea50859a16dbb3e
Matt Galloway <matt@galloway.me.uk>
2016-09-23 20:25:35 +0100

    Add the project outline for a watch app

---
+++ EmonCMSWatch Extension/InterfaceController.swift
@@ -0,0 +1,27 @@
+//
+//  InterfaceController.swift
+//  EmonCMSWatch Extension
+//
+//  Created by Matt Galloway on 23/09/2016.
+//  Copyright © 2016 Matt Galloway. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+  override func awake(withContext context: Any?) {
+    super.awake(withContext: context)
+  }
+
+  override func willActivate() {
+    super.willActivate()
+  }
+
+  override func didDeactivate() {
+    super.didDeactivate()
+  }
+
+}
