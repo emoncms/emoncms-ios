ae1461841689b3e3fbde23a4e3bd8f3461f8366e
Matt Galloway <matt@galloway.me.uk>
2016-09-23 23:01:14 +0100

    Setting up the watch complications

--- EmonCMSWatch Extension/Controllers/ComplicationController.swift
+++ EmonCMSWatch Extension/Controllers/ComplicationController.swift
@@ -11,34 +11,68 @@ import ClockKit
 
 class ComplicationController: NSObject, CLKComplicationDataSource {
 
-  // MARK: - Timeline Configuration
-
   func getSupportedTimeTravelDirections(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTimeTravelDirections) -> Void) {
-    handler([.backward])
-  }
-
-  func getTimelineStartDate(for complication: CLKComplication, withHandler handler: @escaping (Date?) -> Void) {
-    handler(nil)
+    handler([])
   }
 
   func getPrivacyBehavior(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationPrivacyBehavior) -> Void) {
     handler(.hideOnLockScreen)
   }
 
-  // MARK: - Timeline Population
-
   func getCurrentTimelineEntry(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTimelineEntry?) -> Void) {
-    handler(nil)
+    let template = self.template(for: complication, feed: FeedViewModel())
+    handler(CLKComplicationTimelineEntry(date: Date(), complicationTemplate: template))
   }
-
-  func getTimelineEntries(for complication: CLKComplication, before date: Date, limit: Int, withHandler handler: @escaping ([CLKComplicationTimelineEntry]?) -> Void) {
-    handler(nil)
-  }
-
-  // MARK: - Placeholder Templates
 
   func getLocalizableSampleTemplate(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTemplate?) -> Void) {
-    handler(nil)
+    let template = self.template(for: complication, feed: FeedViewModel())
+    handler(template)
+  }
+
+  private func template(for complication: CLKComplication, feed: FeedViewModel) -> CLKComplicationTemplate {
+    let template: CLKComplicationTemplate
+
+    let name = CLKSimpleTextProvider(text: feed.name)
+    let value = CLKSimpleTextProvider(text: feed.value)
+
+    switch complication.family {
+    case .modularSmall:
+      let t = CLKComplicationTemplateModularSmallStackText()
+      t.line1TextProvider = name
+      t.line2TextProvider = value
+      template = t
+    case .modularLarge:
+      let t = CLKComplicationTemplateModularLargeTallBody()
+      t.headerTextProvider = name
+      t.bodyTextProvider = value
+      template = t
+    case .circularSmall:
+      let t = CLKComplicationTemplateCircularSmallStackText()
+      t.line1TextProvider = name
+      t.line2TextProvider = value
+      template = t
+    case .extraLarge:
+      let t = CLKComplicationTemplateExtraLargeStackText()
+      t.line1TextProvider = name
+      t.line2TextProvider = value
+      template = t
+    case .utilitarianLarge:
+      let t = CLKComplicationTemplateUtilitarianLargeFlat()
+      t.textProvider = value
+      t.imageProvider = CLKImageProvider(onePieceImage: UIImage.init(named: "Complication/Utilitarian")!)
+      template = t
+    case .utilitarianSmall:
+      let t = CLKComplicationTemplateUtilitarianSmallSquare()
+      t.imageProvider = CLKImageProvider(onePieceImage: UIImage.init(named: "Complication/Circular")!)
+      template = t
+    case .utilitarianSmallFlat:
+      let t = CLKComplicationTemplateUtilitarianSmallFlat()
+      t.textProvider = value
+      t.imageProvider = CLKImageProvider(onePieceImage: UIImage.init(named: "Complication/Circular")!)
+      template = t
+    }
+
+    return template
   }
 
 }
