145ba8eb92313c4379dacb49deb726a36787bfad
Matt Galloway <matt@galloway.me.uk>
2019-01-14 15:48:30 +0000

    Add Realm migration for the apps

--- EmonCMSiOS/Model/Account.swift
+++ EmonCMSiOS/Model/Account.swift
@@ -22,6 +22,11 @@ struct Account {
     let fileURL = container.appendingPathComponent(self.uuid.uuidString + ".realm")
     var config = Realm.Configuration(fileURL: fileURL)
     config.schemaVersion = 1
+    config.migrationBlock = { (migration, oldSchemaVersion) in
+      if oldSchemaVersion == 0 {
+        self.migrate_0_1(migration)
+      }
+    }
 
     #if DEBUG
     config.deleteRealmIfMigrationNeeded = true
@@ -50,6 +55,33 @@ extension Account: Equatable {
     return lhs.uuid == rhs.uuid &&
       lhs.url == rhs.url &&
       lhs.apikey == rhs.apikey
+  }
+
+}
+
+extension Account {
+
+  private func migrate_0_1(_ migration: Migration) {
+    // Migrate the apps to new data model
+    migration.enumerateObjects(ofType: "MyElectricAppData") { (oldObject, newObject) in
+      guard let oldObject = oldObject else { return }
+
+      guard
+        let uuid = oldObject.value(forKey: "uuid"),
+        let name = oldObject.value(forKey: "name"),
+        let useFeedId = oldObject.value(forKey: "useFeedId"),
+        let kwhFeedId = oldObject.value(forKey: "kwhFeedId"),
+        let feedsJsonData = try? JSONSerialization.data(withJSONObject: ["use":useFeedId, "kwh":kwhFeedId], options: [])
+        else {
+          return
+      }
+
+      let newAppData = migration.create("AppData")
+      newAppData.setValue(uuid, forKey: "uuid")
+      newAppData.setValue(name, forKey: "name")
+      newAppData.setValue(AppCategory.myElectric.rawValue, forKey: "category")
+      newAppData.setValue(feedsJsonData, forKey: "feedsJson")
+    }
   }
 
 }
