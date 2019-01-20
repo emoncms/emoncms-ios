563634d12e965f8a8e556a78d3a7274ad13650ee
Matt Galloway <matt@galloway.me.uk>
2019-01-20 21:58:25 +0000

    Add KeychainController tests

---
+++ EmonCMSiOSTests/KeychainControllerTests.swift
@@ -0,0 +1,62 @@
+//
+//  KeychainControllerTests.swift
+//  EmonCMSiOSTests
+//
+//  Created by Matt Galloway on 20/01/2019.
+//  Copyright © 2019 Matt Galloway. All rights reserved.
+//
+
+import Quick
+import Nimble
+@testable import EmonCMSiOS
+
+class KeychainControllerTests: QuickSpec {
+
+  override func spec() {
+
+    var controller: KeychainController!
+
+    beforeEach {
+      controller = KeychainController()
+    }
+
+    describe("keychainController") {
+      it("should save account") {
+        let accountId = "test_account"
+        let apiKey = "test"
+        expect { try controller.logout(ofAccountWithId: accountId) }
+          .toNot(throwError())
+        expect { try controller.saveAccount(forId: accountId, apiKey: apiKey) }
+          .toNot(throwError())
+        expect(controller.apiKey(forAccountWithId: accountId)).to(equal(apiKey))
+      }
+
+      it("should logout of account") {
+        let accountId = "test_account"
+        let apiKey = "test"
+        expect { try controller.logout(ofAccountWithId: accountId) }
+          .toNot(throwError())
+        expect { try controller.saveAccount(forId: accountId, apiKey: apiKey) }
+          .toNot(throwError())
+        expect { try controller.logout(ofAccountWithId: accountId) }
+          .toNot(throwError())
+        expect(controller.apiKey(forAccountWithId: accountId)).to(beNil())
+      }
+
+      it("should update account if already exists") {
+        let accountId = "test_account"
+        let apiKey1 = "test1"
+        let apiKey2 = "test2"
+        expect { try controller.logout(ofAccountWithId: accountId) }
+          .toNot(throwError())
+        expect { try controller.saveAccount(forId: accountId, apiKey: apiKey1) }
+          .toNot(throwError())
+        expect { try controller.saveAccount(forId: accountId, apiKey: apiKey2) }
+          .toNot(throwError())
+        expect(controller.apiKey(forAccountWithId: accountId)).to(equal(apiKey2))
+      }
+    }
+
+  }
+
+}
