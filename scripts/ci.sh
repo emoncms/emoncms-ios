6ec54af9a27e724015adf8bec49e675b3fec7257
Matt Galloway <matt@galloway.me.uk>
2019-01-26 12:07:14 +0000

    Better xcpretty for travis

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -2,5 +2,7 @@
 
 set -e
 
-scripts/intelligent-bootstrap | xcpretty
-xcodebuild -project EmonCMSiOS.xcodeproj -scheme EmonCMSiOS -destination "platform=iOS Simulator,name=iPhone XR" CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO clean build test | xcpretty && exit ${PIPESTATUS[0]}
+XCPRETTY="xcpretty -f `xcpretty-travis-formatter`"
+
+scripts/intelligent-bootstrap | ${XCPRETTY}
+xcodebuild -project EmonCMSiOS.xcodeproj -scheme EmonCMSiOS -destination "platform=iOS Simulator,name=iPhone XR" CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO clean build test | ${XCPRETTY} && exit ${PIPESTATUS[0]}
