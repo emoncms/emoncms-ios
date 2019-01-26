4f239032dac5e5ee5e652bea05feb17fa29e8cbe
Matt Galloway <matt@galloway.me.uk>
2019-01-26 11:24:19 +0000

    Attempt to fix build

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-make bootstrap
+scripts/intelligent-bootstrap
 xcodebuild -project EmonCMSiOS.xcodeproj -scheme EmonCMSiOS -destination "platform=iOS Simulator,name=iPhone XR" CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO clean build test | xcpretty && exit ${PIPESTATUS[0]}
