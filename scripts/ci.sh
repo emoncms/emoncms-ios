d4d2b8ae16861007ee5d178a46b6433dad6fbd78
Matt Galloway <matt@galloway.me.uk>
2019-01-26 12:57:56 +0000

    Don't pipe carthage to xcpretty

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -4,5 +4,5 @@ set -e
 
 XCPRETTY="xcpretty -f `xcpretty-travis-formatter`"
 
-scripts/intelligent-bootstrap | ${XCPRETTY}
+scripts/intelligent-bootstrap
 xcodebuild -project EmonCMSiOS.xcodeproj -scheme EmonCMSiOS -destination "platform=iOS Simulator,name=iPhone XR" CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO clean build test | ${XCPRETTY} && exit ${PIPESTATUS[0]}
