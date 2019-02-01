0fe9c1f8d90980b8564182752a7717b6a9830ade
Matt Galloway <matt@galloway.me.uk>
2019-02-01 23:05:14 +0000

    Also test on iPad Air 2 now we are universal

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -5,4 +5,12 @@ set -e
 XCPRETTY="xcpretty -f `xcpretty-travis-formatter`"
 
 scripts/intelligent-bootstrap
-xcodebuild -project EmonCMSiOS.xcodeproj -scheme EmonCMSiOS -destination "platform=iOS Simulator,name=iPhone XR" CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO clean build test | ${XCPRETTY} && exit ${PIPESTATUS[0]}
+xcodebuild \
+	-project EmonCMSiOS.xcodeproj \
+	-scheme EmonCMSiOS \
+	-destination "platform=iOS Simulator,name=iPhone XR" \
+	-destination "platform=iOS Simulator,name=iPad Air 2" \
+	CODE_SIGN_IDENTITY="" \
+	CODE_SIGNING_REQUIRED=NO \
+	clean build test \
+	| ${XCPRETTY} && exit ${PIPESTATUS[0]}
