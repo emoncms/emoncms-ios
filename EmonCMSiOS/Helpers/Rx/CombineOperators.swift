760378c6ff385160dcaa91f6f8cf24351a1d1b0a
Matt Galloway <matt@galloway.me.uk>
2020-02-16 14:21:08 +0000

    Expand Producer to send a subscription when subscribed

--- EmonCMSiOS/Helpers/Rx/CombineOperators.swift
+++ EmonCMSiOS/Helpers/Rx/CombineOperators.swift
@@ -17,23 +17,58 @@ extension Publisher {
 
 }
 
-// TAKEN FROM: https://twitter.com/peres/status/1159972724577583110
+// INSPIRED BY: https://twitter.com/peres/status/1159972724577583110
 struct Producer<T, E: Error>: Publisher {
   typealias Output = T
   typealias Failure = E
 
-  private let handler: (AnySubscriber<T, E>) -> Void
+  private let handler: (Producer.Subscriber) -> Void
 
-  init(_ handler: @escaping (AnySubscriber<T, E>) -> Void) {
+  init(_ handler: @escaping (Producer.Subscriber) -> Void) {
     self.handler = handler
   }
 
   func receive<Downstream>(subscriber: Downstream)
     where
-    Downstream: Subscriber,
+    Downstream: Combine.Subscriber,
     E == Downstream.Failure,
     T == Downstream.Input {
-      let wrap = AnySubscriber(subscriber)
+      let wrap = Producer.Subscriber(downstream: AnySubscriber(subscriber))
+      let subscription = Producer.Subscription(subscriber: wrap)
+      subscriber.receive(subscription: subscription)
       handler(wrap)
+  }
+
+  public class Subscriber {
+    private var downstream: AnySubscriber<T, E>
+    fileprivate var cancelled = false
+
+    init(downstream: AnySubscriber<T, E>) {
+      self.downstream = downstream
+    }
+
+    func receive(_ value: T) -> Subscribers.Demand {
+      return self.downstream.receive(value)
+    }
+
+    func receive(completion: Subscribers.Completion<E>) {
+      self.downstream.receive(completion: completion)
+    }
+  }
+
+  private class Subscription: Combine.Subscription {
+    var subscriber: Producer.Subscriber?
+
+    init(subscriber: Producer.Subscriber) {
+      self.subscriber = subscriber
+    }
+
+    func request(_ demand: Subscribers.Demand) {
+    }
+
+    func cancel() {
+      self.subscriber?.cancelled = true
+      self.subscriber = nil
+    }
   }
 }
