8b945c437e2e13cc0171db695f3ba20146cd002b
Matt Galloway <matt@galloway.me.uk>
2020-09-27 16:42:11 +0100

    Add feed cell and input cell tests

---
+++ EmonCMSiOSTests/SnapshotTests/FeedCellTests.swift
@@ -0,0 +1,87 @@
+//
+//  FeedCellTests.swift
+//  EmonCMSiOSTests
+//
+//  Created by Matt Galloway on 27/09/2020.
+//  Copyright © 2020 Matt Galloway. All rights reserved.
+//
+
+@testable import EmonCMSiOS
+import Nimble
+import Quick
+import Realm
+import RealmSwift
+import SnapshotTesting
+import UIKit
+
+class FeedCellTests: EmonCMSTestCase {
+  private var cellSetup: (FeedCell) -> Void = { _ in }
+
+  override func setUp() {
+    super.setUp()
+    isRecording = false
+  }
+
+  override func spec() {
+    var tableView: UITableView!
+
+    beforeEach {
+      tableView = UITableView(frame: CGRect(x: 0, y: 0, width: 375, height: 700), style: .plain)
+      tableView.dataSource = self
+      tableView.delegate = self
+      tableView.register(UINib(nibName: "FeedCell", bundle: nil), forCellReuseIdentifier: "FeedCell")
+
+      self.cellSetup = { _ in }
+    }
+
+    describe("feedCell") {
+      it("Should display normally") {
+        self.cellSetup = { cell in
+          cell.titleLabel.text = "Feed Name"
+          cell.valueLabel.text = "123"
+          cell.timeLabel.text = "10 seconds ago"
+          cell.activityCircle.backgroundColor = EmonCMSColors.ActivityIndicator.Green
+          cell.chartViewModel.send(nil)
+        }
+        assertSnapshot(matching: tableView, as: .image(traits: .init(userInterfaceStyle: .light)))
+      }
+
+      it("Should display expanded") {
+        self.cellSetup = { cell in
+          cell.titleLabel.text = "Feed Name"
+          cell.valueLabel.text = "123"
+          cell.timeLabel.text = "10 seconds ago"
+          cell.activityCircle.backgroundColor = EmonCMSColors.ActivityIndicator.Green
+          cell.chartViewModel.send(self.makeFeedChartViewModel())
+        }
+        assertSnapshot(matching: tableView, as: .image(traits: .init(userInterfaceStyle: .light)))
+      }
+    }
+  }
+
+  private func makeFeedChartViewModel() -> FeedChartViewModel {
+    let realmController = RealmController(dataDirectory: self.dataDirectory)
+    let credentials = AccountCredentials(url: "https://test", apiKey: "ilikecats")
+    let accountController = AccountController(uuid: "testaccount-\(type(of: self))", credentials: credentials)
+    let realm = realmController.createAccountRealm(forAccountId: accountController.uuid)
+    try! realm.write {
+      realm.deleteAll()
+    }
+
+    let requestProvider = MockHTTPRequestProvider()
+    let api = EmonCMSAPI(requestProvider: requestProvider)
+    return FeedChartViewModel(account: accountController, api: api, feedId: "1")
+  }
+}
+
+extension FeedCellTests: UITableViewDataSource, UITableViewDelegate {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 1
+  }
+
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "FeedCell", for: indexPath) as! FeedCell
+    self.cellSetup(cell)
+    return cell
+  }
+}
---
+++ EmonCMSiOSTests/SnapshotTests/InputCellTests.swift
@@ -0,0 +1,59 @@
+//
+//  InputCellTests.swift
+//  EmonCMSiOSTests
+//
+//  Created by Matt Galloway on 27/09/2020.
+//  Copyright © 2020 Matt Galloway. All rights reserved.
+//
+
+@testable import EmonCMSiOS
+import Nimble
+import Quick
+import SnapshotTesting
+import UIKit
+
+class InputCellTests: EmonCMSTestCase {
+  private var cellSetup: (InputCell) -> Void = { _ in }
+
+  override func setUp() {
+    super.setUp()
+    isRecording = false
+  }
+
+  override func spec() {
+    var tableView: UITableView!
+
+    beforeEach {
+      tableView = UITableView(frame: CGRect(x: 0, y: 0, width: 375, height: 700), style: .plain)
+      tableView.dataSource = self
+      tableView.delegate = self
+      tableView.register(UINib(nibName: "InputCell", bundle: nil), forCellReuseIdentifier: "InputCell")
+
+      self.cellSetup = { _ in }
+    }
+
+    describe("inputCell") {
+      it("Should display normally") {
+        self.cellSetup = { cell in
+          cell.titleLabel.text = "Input Name"
+          cell.valueLabel.text = "123"
+          cell.timeLabel.text = "10 seconds ago"
+          cell.activityCircle.backgroundColor = EmonCMSColors.ActivityIndicator.Green
+        }
+        assertSnapshot(matching: tableView, as: .image(traits: .init(userInterfaceStyle: .light)))
+      }
+    }
+  }
+}
+
+extension InputCellTests: UITableViewDataSource, UITableViewDelegate {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 1
+  }
+
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "InputCell", for: indexPath) as! InputCell
+    self.cellSetup(cell)
+    return cell
+  }
+}
